3d5403c1ef7b9f6768fab37bc9bb638e805eeddf
zhouandrewc <zhou@Andrews-MacBook-Pro-37.local>
2016-12-04 02:17:33 -0500

    added example scripts

--- script-generic2.sh
+++ script-generic2.sh
@@ -7,15 +7,14 @@
 #SBATCH -p serial_requeue       # Partition to submit to
 #SBATCH --mem=20000               # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH --constraint=cuda-7.5
-#SBATCH -o script-generic.out      # File to which STDOUT will be written
-#SBATCH -e script-generic.err      # File to which STDERR will be written
+#SBATCH -o theano-3.out      # File to which STDOUT will be written
+#SBATCH -e theano-3.err      # File to which STDERR will be written
 #SBATCH --mail-type=ALL         # Type of email notification- BEGIN,END,FAIL,ALL
 #SBATCH --mail-user=andrewzhou@college.harvard.edu # Email to which notifications will be sent
  
 hostname
 module load python
 module load cuda/7.5-fasrc01
-
 
 filename="4-mod"
 weightfile="sherlock-weightfile"
@@ -26,4 +25,4 @@ batchsize="64"
 dropout="0.2"
 
 source activate mypython
-KERAS_BACKEND=theano THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32 python src/train-generic.py -t $filename -w $weightfile -l $layers -n $nodes -e $epochs -b $batchsize -d $dropout
+KERAS_BACKEND=theano THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32 python src/train-wonderland-3.py -t $filename -w $weightfile -l $layers -n $nodes -e $epochs -b $batchsize -d $dropout
